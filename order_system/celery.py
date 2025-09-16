6c4671e08617e0162b550a494bb1f8103fe09414
Addy Mutuiri <123188947+mutuiris@users.noreply.github.com>
2025-09-16 20:18:51 +0300

    feat: add Celery configuration for background task processing

---
+++ order_system/celery.py
@@ -0,0 +1,20 @@
+"""
+Celery configuration for background task processing
+Handles async SMS and email notifications
+"""
+
+import os
+from celery import Celery
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'order_system.settings')
+
+app = Celery('order_system')
+
+app.config_from_object('django.conf:settings', namespace='CELERY')
+
+app.autodiscover_tasks()
+
+@app.task(bind=True)
+def debug_task(self):
+    """Debug task to verify Celery is working"""
+    print(f'Request: {self.request!r}')
