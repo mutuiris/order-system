f98251c1d34a63584f054a4e9f857142204e7245
Addy Mutuiri <123188947+mutuiris@users.noreply.github.com>
2025-09-17 17:53:10 +0300

    feat: add test settings for faster testing with in-memory database and disabled migrations

---
+++ order_system/test_settings.py
@@ -0,0 +1,31 @@
+from .settings import *
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': ':memory:',
+        'TEST': {
+            'NAME': ':memory:',
+        },
+    }
+}
+
+# Speed up password hashing for tests
+PASSWORD_HASHERS = [
+    'django.contrib.auth.hashers.MD5PasswordHasher',
+]
+
+# Disable migrations for faster tests
+class DisableMigrations:
+    def __contains__(self, item):
+        return True
+
+    def __getitem__(self, item):
+        return None
+
+MIGRATION_MODULES = DisableMigrations()
+
+# Disable logging during tests
+LOGGING_CONFIG = None
+import logging
+logging.disable(logging.CRITICAL)
